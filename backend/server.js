43fd6244008c384f0073eba1ad5aad0eb7ccb128
Lucas Bandeira <lucasbandeira.mj@outlook.com>
2021-02-19 22:00:48 -0300

    added colors to my terminal

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import colors from 'colors'
 
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 dotenv.config()
+connectDB()
+
 const app = express() 
 app.use(cors())
 
@@ -20,4 +24,4 @@ app.get('/api/products/:id', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`.cyan))
