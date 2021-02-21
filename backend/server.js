5e78c2c5f6d8a8b068adb10d3b78e3b031c94f1a
Lucas Bandeira <lucasbandeira.mj@outlook.com>
2021-02-21 17:27:01 -0300

    added middleware folder and error messages

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,10 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import colors from 'colors'
-
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 connectDB()
@@ -12,15 +13,11 @@ connectDB()
 const app = express() 
 app.use(cors())
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id)
-
-    res.json(product)
-})
+// Error handling
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000;
 
