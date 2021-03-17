70a6c4c652a7794edaf52ab2fbfbd63d0a016a80
Lucas Bandeira <lucasbandeira.mj@outlook.com>
2021-03-17 10:12:23 -0300

    added cart page and functionalities

---
+++ frontend/src/reducers/cartReducers.js
@@ -0,0 +1,28 @@
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
+
+export const cartReducer = ( state = { cartItems: [] }, action ) => {
+    switch(action.type) {
+        case CART_ADD_ITEM:
+            const item = action.payload
+            const itemExists = state.cartItems.find(x => x.product === item.product)
+
+            if(itemExists) {
+                return {
+                    ...state,
+                    cartItems: state.cartItems.map(x => x.product === itemExists.product ? item : x )
+                }
+            } else {
+                return {
+                    ...state,
+                    cartItems: [...state.cartItems, item]
+                }
+            }
+        case CART_REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(x => x.product !== action.payload),
+            }
+        default: 
+            return state
+    }
+}
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -14,7 +14,7 @@ import { listProductDetails } from '../actions/productActions'
 const ProductScreen = ({ history, match }) => {
     const dispatch = useDispatch()
 
-    const [qty, setQty] = useState(0)
+    const [qty, setQty] = useState(1)
 
     const productDetails = useSelector(state => state.productDetails)
     const { loading, error, product} = productDetails
