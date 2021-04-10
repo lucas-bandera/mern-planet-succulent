29d6f58026b9434ca036c96cfe188709ca3406cc
Lucas Bandeira <lucasbandeira.mj@outlook.com>
2021-04-10 14:21:40 -0300

    added user registration backend function

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 const router = express.Router()
-import { authUser, getUserProfile } from '../controllers/userController.js'
+import { authUser, getUserProfile, registerUser } from '../controllers/userController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 // @desc    Auth user & get token
@@ -8,6 +8,14 @@ import { protect } from '../middleware/authMiddleware.js'
 // @access  Public
 router.post('/login', authUser)
 
+// @desc    Register new user & get token
+// @route   POST /api/users
+// @access  Public
+router.route('/').post(registerUser)
+
+// @desc    GET user profile
+// @route   GET /api/users/profile
+// @access  Private
 router.route('/profile')
     .get(protect, getUserProfile)
 
